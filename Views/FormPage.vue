22e7f8e1e20c1e2d5019ba664ae20cfa26bdf1b8
GersonMzLOL <mgerso777@gmail.com>
2025-10-06 19:51:27 -0600

    Elaboracion de Formulario

---
+++ Views/FormPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <form>
+        <div>
+            <label for="nombre">Nombre</label>
+            <input id="nombre" name="nombre" type="text"/>
+        </div>
+
+        <div>
+            <label for="direccion">Direccion</label>
+            <input id="direccion" name="direccion" type="text"/>
+        </div>
+
+        <div>
+            <label for="edad">Edad</label>
+            <input id="edad" name="edad" type="number"/>
+        </div>
+
+        <div>
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'FormPage',
+};
+</script>
